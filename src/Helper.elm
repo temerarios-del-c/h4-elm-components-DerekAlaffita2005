a07bbdf7cb73e7cc603e35351966dfae3c6338c0
DerekAlaffita2005 <up230043@alumnos.upa.edu.mx>
2026-02-22 20:10:14 -0600

    H4 Derek ISC07C

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,38 +1,36 @@
-module Helper exposing (..)
+module Helper exposing (evalChars, headers, hyperlink, isUpperChars, joinWords)
 
-import Html exposing (Html, div, h1, h2, h3, h4, h5, h6, a, text)
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
 import Html.Attributes exposing (href)
 
 
 joinWords : String -> String -> String
-joinWords word1 word2 =
-    word1 ++ word2
+joinWords str1 str2 =
+    str1 ++ str2
 
 
 isUpperChars : List Char -> List Bool
-isUpperChars list =
-    List.map Char.isUpper list
-
+isUpperChars chars =
+    List.map Char.isUpper chars
 
 
 evalChars : List Char -> (Char -> Bool) -> List Bool
-evalChars list funTrans =
-    List.map funTrans list
-
+evalChars chars predicate =
+    List.map predicate chars
 
 
 headers : String -> Html msg
-headers param =
+headers content =
     div []
-        [ h1 [] [ text param ]
-        , h2 [] [ text param ]
-        , h3 [] [ text param ]
-        , h4 [] [ text param ]
-        , h5 [] [ text param ]
-        , h6 [] [ text param ]
+        [ h1 [] [ text content ]
+        , h2 [] [ text content ]
+        , h3 [] [ text content ]
+        , h4 [] [ text content ]
+        , h5 [] [ text content ]
+        , h6 [] [ text content ]
         ]
 
 
 hyperlink : String -> String -> Html msg
-hyperlink url label =
-    a [ href url ] [ text label ]
+hyperlink url linkText =
+    a [ href url ] [ text linkText ]
